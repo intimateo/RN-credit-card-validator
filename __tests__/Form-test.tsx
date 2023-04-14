233ea96308a957836e31d7d153e8faee752189b2
Inti Mateo <inti@imateo.com>
2023-04-14 17:15:33 -0500

    perform some improvements on test

--- __tests__/Form-test.tsx
+++ __tests__/Form-test.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react-native';
+import { render, fireEvent, waitFor, screen, act } from '@testing-library/react-native';
 import Form from '../src/Components/CreditCardForm';
 
 describe('Form', () => {
+    let form, cardNumberInput, cardExpiryInput, cardCvcInput, cardFirstNameInput, cardLastNameInput, submitButton;
     beforeEach(() => {
-        render(<Form />);
+        form = render(<Form />);
+        const {getByTestId} = form;
+        cardNumberInput = getByTestId('card-number-input');
+        cardExpiryInput = getByTestId('card-expiration-date-input');
+        cardCvcInput = getByTestId('card-cvv-code-input');
+        cardFirstNameInput = getByTestId('card-first-name-input');
+        cardLastNameInput = getByTestId('card-last-name-input');
+        submitButton = getByTestId('submit-button');
       });
+
+    afterEach(() => {
+        form.unmount();
+    });
     
     it('renders all necessary components', () => {
-      expect(screen.getByTestId('card-number-input')).toBeDefined();
-      expect(screen.getByTestId('card-expiration-date-input')).toBeDefined();
-      expect(screen.getByTestId('card-cvv-code-input')).toBeDefined();
-      expect(screen.getByTestId('submit-button')).toBeDefined();
-      expect(screen.getByTestId('card-first-name-input')).toBeDefined();
-      expect(screen.getByTestId('card-last-name-input')).toBeDefined();
+      expect(cardNumberInput).toBeDefined();
+      expect(cardExpiryInput).toBeDefined();
+      expect(cardCvcInput).toBeDefined();
+      expect(submitButton).toBeDefined();
+      expect(cardFirstNameInput).toBeDefined();
+      expect(cardLastNameInput).toBeDefined();
     });
   
     it('updates input values on change', () => {
-      const cardNumberInput = screen.getByTestId('card-number-input');
-      const cardExpiryInput = screen.getByTestId('card-expiration-date-input');
-      const cardCvcInput = screen.getByTestId('card-cvv-code-input');
-      const cardFirstNameInput = screen.getByTestId('card-first-name-input');
-      const cardLastNameInput = screen.getByTestId('card-last-name-input')
-  
       fireEvent.changeText(cardNumberInput, '1234 5678 9012 3456');
       fireEvent.changeText(cardExpiryInput, '12/24');
       fireEvent.changeText(cardCvcInput, '123');
@@ -36,10 +42,8 @@ describe('Form', () => {
       expect(cardLastNameInput.props.value).toBe('Doe');
     });
 
-    it('should validate credit card number after submitting the form', async () => {
-        const cardNumberInput = screen.getByTestId('card-number-input');
-        const submitButton = screen.getByTestId('submit-button');
-    
+    test('validates credit card number after submitting the form', async () => {
+        jest.useFakeTimers();
         // Enter valid credit card number
         fireEvent.changeText(cardNumberInput, '4111111111111111');
     
@@ -51,52 +55,45 @@ describe('Form', () => {
 
         // Enter a not valid credit card number and shows the error message
         fireEvent.changeText(cardNumberInput, '378282246310005');
-        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeTruthy()},{timeout: 2000});
+        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeTruthy()});
 
         // Enter a Visa card number
         fireEvent.changeText(cardNumberInput, '4242424242424242');
-        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeFalsy()},{timeout: 2000});
+        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeFalsy()});
 
         // Enter a Mastercard card number
         fireEvent.changeText(cardNumberInput, '5555555555554444');
-        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeFalsy()},{timeout: 2000});
+        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeFalsy()});
 
         // Enter a Amex card number
         fireEvent.changeText(cardNumberInput, '378282246310005');
-        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeFalsy()},{timeout: 2000});
+        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeFalsy()});
 
         // Enter a Discover card number
         fireEvent.changeText(cardNumberInput, '6011111111111117');
-        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeFalsy()},{timeout: 2000});
+        await waitFor(() => {expect(screen.queryByTestId("error-card-number")).toBeFalsy()});
+        jest.runAllTimers();
       });
 
       it('should validate an expired date', async () => {
-        const cardExpiryInput = screen.getByTestId('card-expiration-date-input');
-        const submitButton = screen.getByTestId('submit-button');
         fireEvent.changeText(cardExpiryInput, '03/23');
         fireEvent.press(submitButton);
         await waitFor(() => {expect(screen.queryByTestId("error-expire-date")).toBeTruthy()},{timeout: 2000});
       })
 
       it('should validate a valid expiration date', async () => {
-        const cardExpiryInput = screen.getByTestId('card-expiration-date-input');
-        const submitButton = screen.getByTestId('submit-button');
         fireEvent.changeText(cardExpiryInput, '08/25');
         fireEvent.press(submitButton);
         await waitFor(() => {expect(screen.queryByTestId("error-expire-date")).toBeFalsy()},{timeout: 2000});
       })
 
       it('should validate only digits and respect the format MM/YY', async () => {
-        const cardExpiryInput = screen.getByTestId('card-expiration-date-input');
-        const submitButton = screen.getByTestId('submit-button');
         fireEvent.changeText(cardExpiryInput, 'abc/25');
         fireEvent.press(submitButton);
         await waitFor(() => {expect(screen.queryByTestId("error-expire-date")).toBeTruthy()},{timeout: 2000});
       })
 
       it('should validate the CVV code could be 3 or 4 digits', async () => {
-        const cardCvcInput = screen.getByTestId('card-cvv-code-input');
-        const submitButton = screen.getByTestId('submit-button');
         fireEvent.changeText(cardCvcInput, 'abc');
         fireEvent.press(submitButton);
         await waitFor(() => {expect(screen.queryByTestId("error-cvv-code")).toBeTruthy()},{timeout: 2000});
@@ -106,14 +103,6 @@ describe('Form', () => {
       })
 
       it('Test submit the form with sucess', async () => {
-        const cardNumberInput = screen.getByTestId('card-number-input');
-        const cardExpiryInput = screen.getByTestId('card-expiration-date-input');
-        const cardCvcInput = screen.getByTestId('card-cvv-code-input');
-        const cardFirstNameInput = screen.getByTestId('card-first-name-input');
-        const cardLastNameInput = screen.getByTestId('card-last-name-input');
-        const submitButton = screen.getByTestId('submit-button');
-
-    
         fireEvent.changeText(cardNumberInput, '1234 5678 9012 3456');
         fireEvent.changeText(cardExpiryInput, '12/24');
         fireEvent.changeText(cardCvcInput, '123');
